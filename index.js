b594b25038131b359c40166379cb8ebb8ae960de
neverneeth <navaneethvsankar@gmail.com>
2025-08-24 09:09:22 +0530

    passes all testcases

--- index.js
+++ index.js
@@ -26,6 +26,30 @@ app.get("/api/hello", function (req, res) {
 
 
 
+app.get("/api/:date?", function (req, res) {
+  let dateString = req.params.date;
+
+  let date;
+  if (!dateString) {
+    date = new Date();
+  } else if (!isNaN(dateString)) {
+    date = new Date(parseInt(dateString));
+  } else {
+    date = new Date(dateString);
+  }
+
+  if (date.toString() === "Invalid Date") {
+    res.json({ error: "Invalid Date" });
+  } else {
+    res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString(),
+    });
+  }
+});
+
+
+
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
